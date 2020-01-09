59929a005a01f813d80fb44b5371e5ba421828bc
Peter Schneider <peter.schneider@kontron.com>
2020-01-09 10:42:22 +0100

    fix model-install.sh
    Signed-off-by: Peter Schneider <peter.schneider@kontron.com>

--- scripts/model-install.sh
+++ scripts/model-install.sh
@@ -6,6 +6,9 @@ shopt -s failglob
 
 local_path=$(dirname $0)
 
+[ -n "{${1}" ] && SYSREPOCTL="${1}"
+[ -n "{${2}" ] && SYSREPOCTL_ROOT_PERMS="${2}"
+
 : ${SYSREPOCTL:=sysrepoctl}
 : ${SYSREPOCTL_ROOT_PERMS:=-o root:root -p 600}
 : ${YANG_DIR:=$local_path/../modules}
@@ -21,7 +24,11 @@ install_yang_module() {
 
     if ! is_yang_module_installed $module; then
         echo "- Installing module $module..."
-        $SYSREPOCTL -i -g ${YANG_DIR}/$module.yang $SYSREPOCTL_ROOT_PERMS
+echo "SYSREPOCTL=\"${SYSREPOCTL}\""
+echo "SYSREPOCTL_ROOT_PERMS=\"${SYSREPOCTL_ROOT_PERMS}\""
+echo "YANG_DIR=\"${YANG_DIR}\""
+        echo "\"$SYSREPOCTL -i ${YANG_DIR}/$module.yang ${SYSREPOCTL_ROOT_PERMS}\""
+        $SYSREPOCTL -i ${YANG_DIR}/$module.yang ${SYSREPOCTL_ROOT_PERMS}
     else
         echo "- Module $module already installed."
     fi
